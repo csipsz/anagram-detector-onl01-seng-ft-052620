bc110c20433c99699b0e40fdb3dfa9903690fad1
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-31 14:10:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array_of_matches)
+    array_of_matches.select do |matches| 
+      if matches.split("").sort == @word.split("").sort
+        matches 
+      end 
+    end 
+  end 
+  
+end 
