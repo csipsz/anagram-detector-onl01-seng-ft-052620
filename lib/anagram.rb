4b0cafa8a26ac96d2f1683f1be6b398ca78aee9d
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-08 01:14:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,1 @@
 require 'pry'
-
-class Anagram 
-  
-  attr_accessor :word 
-  
-  def initialize(word)
-    @word = word 
-  end 
-  
-  def match(array_of_matches)
-    array_of_matches.select do |matches| 
-      if matches.split("").sort == @word.split("").sort
-        matches 
-      end 
-    end 
-  end 
-  
-end 
